9edd00f032fb264f95d03273eb96dd8e4e4f3aff
Alam12346 <syukursidiqnuralam@gmail.com>
2024-10-10 15:23:52 +0700

    Latihan Mengirim Data Antar Fragment

--- MyFlexibleFragment/app/src/main/java/com/task/myflexiblefragment/CategoryFragment.kt
+++ MyFlexibleFragment/app/src/main/java/com/task/myflexiblefragment/CategoryFragment.kt
@@ -9,6 +9,7 @@ import android.widget.Button
 
 class CategoryFragment : Fragment(), View.OnClickListener {
 
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -23,7 +24,21 @@ class CategoryFragment : Fragment(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         if (v?.id == R.id.btn_detail_category) {
+            val detailCategoryFragment = DetailCategoryFragment()
 
+            val bundle = Bundle()
+            bundle.putString(DetailCategoryFragment.EXTRA_NAME, "Lifestyle")
+            val description = "Kategori ini akan berisi produk-produk lifestyle"
+
+            detailCategoryFragment.arguments = bundle
+            detailCategoryFragment.description = description
+
+            val fragmentManager = parentFragmentManager
+            fragmentManager?.beginTransaction()?.apply {
+                replace(R.id.frame_container, detailCategoryFragment, DetailCategoryFragment::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
         }
     }
 }
