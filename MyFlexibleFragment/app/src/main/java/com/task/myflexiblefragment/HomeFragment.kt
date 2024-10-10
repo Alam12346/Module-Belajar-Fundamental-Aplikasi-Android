39caafd70fa1de905e98fb2a1b4924c9681eae8d
Alam12346 <syukursidiqnuralam@gmail.com>
2024-10-10 15:05:39 +0700

    Latihan Flexible Fragment di Satu Activity

--- MyFlexibleFragment/app/src/main/java/com/task/myflexiblefragment/HomeFragment.kt
+++ MyFlexibleFragment/app/src/main/java/com/task/myflexiblefragment/HomeFragment.kt
@@ -18,11 +18,19 @@ class HomeFragment : Fragment(), View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val btnCategory: Button = view.findViewById(R.id.btn_category)
+        val btnCategory:Button = view.findViewById(R.id.btn_category)
         btnCategory.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
-
+        if (v?.id == R.id.btn_category) {
+            val categoryFragment = CategoryFragment()
+            val fragmentManager = parentFragmentManager
+            fragmentManager.beginTransaction().apply {
+                replace(R.id.frame_container, categoryFragment, CategoryFragment::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
